9d3e83a1c0e6b28612b9487c1c33cfaff4872091
Amphakarn <pisuthigomol@yahoo.com>
2021-03-27 19:10:54 +0000

    Set up components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <Navbar></Navbar>
+      <ProductList />
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
     </React.Fragment>
   );
 }
