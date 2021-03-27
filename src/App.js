fe43cf61ec7bfec884e8cbcf6783648fd1842898
Amphakarn <pisuthigomol@yahoo.com>
2021-03-27 19:46:41 +0000

    Install react-router-dom and create routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
@@ -12,11 +13,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <React.Fragment>
-      <Navbar></Navbar>
-      <ProductList />
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={ProductList}></Route>
+        <Route path='/details' component={Details}></Route>
+        <Route path='/cart' component={Cart}></Route>
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
