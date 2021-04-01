6f1325c0e9902f534632ce281ba7fd7903186eaf
Amphakarn <pisuthigomol@yahoo.com>
2021-04-01 22:09:50 +0000

    Clean up

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CartItem({ item, value }) {
   const { id, img, title, price, count, total } = item;
   const { increment, decrement, removeItem } = value;
-  // console.log("VALUE** = ", value);
+
   return (
     <div className='row my-2 text-capitalize text-center'>
       <div className='col-10 mx-auto col-lg-2'>
