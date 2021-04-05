5bfbb1194d753e04b97bcb4f7d5cbc6ae922ece1
Amphakarn <pisuthigomol@yahoo.com>
2021-04-05 20:00:50 +0000

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import CarouselItems from "./CarouselItems";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <CarouselItems></CarouselItems>
+      </div>
+    );
+  }
+}
